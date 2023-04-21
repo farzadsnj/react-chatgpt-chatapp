bcd4c693dc2f5dbcb0371f258773bb257431e6ac
farzad sanjarani <farzadsanjarani3@gmail.com>
2023-04-21 14:08:09 +1000

    co2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Chat from "@/components/chat/Chat"
+
 function App() {
 
   return (
     <div className='app'>
-      app
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
