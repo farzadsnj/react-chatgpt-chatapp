10fb92fb6a54a0d0f82604974ad323ef55160107
farzad sanjarani <farzadsanjarani3@gmail.com>
2023-04-25 11:00:00 +1000

    co8

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from './routes/openAi'
 
 // configuration
 dotenv.config()
@@ -15,6 +17,15 @@ app.use(morgan("common"))
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
+
+// openai config
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
+
+// 
+app.use('/openai', openAiRoutes)
 
 // server setup
 const PORT=process.env.PORT || 9000;
